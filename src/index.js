e9e3f3d6e2df96140dbadcd967a152dacf22fb3a
Trần Tuấn Anh <91464224+Khi137@users.noreply.github.com>
2024-07-09 16:51:46 +0700

    Fix thunk import
    Fix thunk import

--- src/index.js
+++ src/index.js
@@ -4,28 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'; // Import combineReducers
+import { createStore, applyMiddleware, compose } from 'redux';
 import { rootReducer } from './redux/reducer/root.js';
-import {thunk} from 'redux-thunk'; // Import default export 'thunk'
+import {thunk} from 'redux-thunk'; 
 import { StyleProvider } from '@ant-design/cssinjs';
-import { numberReducer } from './DemoRedux/redux/reducer'; // Import numberReducer
+import { numberReducer } from './DemoRedux/redux/reducer';
 
-// Combine reducers
-const rootReducerCombined = combineReducers({
-  root: rootReducer,
-  number: numberReducer, // Tên key 'number' sẽ là state của numberReducer
-});
-
-// DevTools extension setup
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-// Create store with combined reducers and middleware
 export const store = createStore(
-  rootReducerCombined, // Sử dụng rootReducerCombined thay vì rootReducer
-  composeEnhancers(applyMiddleware(thunk))
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk)) 
 );
 
-// Render the app
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <StyleProvider hashPriority="high">
